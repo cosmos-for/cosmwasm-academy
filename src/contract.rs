41a0632adb01e0e3fd2788a501aafb8a46b9e17b
lshoo <lshoo36@gmail.com>
2023-06-27 20:56:24 +0800

    Add contract state

--- src/contract.rs
+++ src/contract.rs
@@ -1,8 +1,20 @@
-pub mod query {
-    use crate::msg::ValueResp;
+use cosmwasm_std::{DepsMut, Response, StdResult};
 
-    pub fn value() -> ValueResp {
-        ValueResp { value: 0 }
+use crate::state::COUNTER;
+
+pub fn instantiate(deps: DepsMut) -> StdResult<Response> {
+    COUNTER.save(deps.storage, &0)?;
+    Ok(Response::new())
+}
+
+pub mod query {
+    use cosmwasm_std::{Deps, StdResult};
+
+    use crate::{msg::ValueResp, state::COUNTER};
+
+    pub fn value(deps: Deps) -> StdResult<ValueResp> {
+        let value = COUNTER.load(deps.storage)?;
+        Ok(ValueResp { value })
     }
 
     pub fn increment(value: u64) -> ValueResp {
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ mod contract;
 
 pub mod msg;
 
+pub mod state;
+
 use cosmwasm_std::{
     entry_point, to_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
 };
@@ -10,12 +12,12 @@ use crate::contract::query;
 
 #[entry_point]
 pub fn instantiate(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
     _info: MessageInfo,
     _msg: Empty,
 ) -> StdResult<Response> {
-    Ok(Response::new())
+    contract::instantiate(deps)
 }
 
 #[entry_point]
@@ -24,11 +26,11 @@ pub fn execute(_deps: DepsMut, _env: Env, _info: MessageInfo, _msg: Empty) -> St
 }
 
 #[entry_point]
-pub fn query(_deps: Deps, _env: Env, msg: msg::QueryMsg) -> StdResult<Binary> {
+pub fn query(deps: Deps, _env: Env, msg: msg::QueryMsg) -> StdResult<Binary> {
     use msg::QueryMsg::*;
 
     match msg {
-        Value {} => to_binary(&query::value()),
+        Value {} => to_binary(&query::value(deps)?),
         Incremented { value } => to_binary(&query::increment(value)),
     }
 }
